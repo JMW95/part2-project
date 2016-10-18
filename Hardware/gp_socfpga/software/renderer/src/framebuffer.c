9020945cce1dbc082b914e00b9f7ca8e94b5e4d6
Jamie Wood <jamie.wood70@ntlworld.com>
2016-10-18 14:30:39 +0100

    Add Clarvi, work queue

---
+++ Hardware/gp_socfpga/software/renderer/src/framebuffer.c
@@ -0,0 +1,77 @@
+#include "framebuffer.h"
+
+static unsigned int bufferaddr = BUFFER1;
+
+void vid_setbuffer(unsigned int addr){
+    bufferaddr = addr;
+}
+
+// plot a single pixel
+void vid_set_pixel(int x, int y, int colour){
+    // derive a pointer to the framebuffer described as 4 bit integers
+    volatile char *framebuffer = (volatile char *) (bufferaddr);
+    
+    // make sure we don't go past the edge of the screen
+    if ((x<0) || (x>DISPLAY_WIDTH-1))
+        return;
+    if ((y<0) || (y>DISPLAY_HEIGHT-1))
+        return;
+    
+    int pixelpos = x+y*DISPLAY_WIDTH;
+    int bufpos = pixelpos/2;
+    
+    char val = framebuffer[bufpos];
+    if((pixelpos & 1) == 0){ // if x even, use lower 4 bits.
+        val = (val & 0xf0) | (colour & 0x0f);
+    }else{ // use upper 4 bits for odd x
+        val = (val & 0x0f) | ((colour & 0x0f) << 4);
+    }
+    
+    framebuffer[bufpos] = val;
+}
+
+// clear the whole screen
+void vid_clear(int colour){
+    volatile int *framebuffer = (volatile int *) (bufferaddr);
+    colour = colour & 0x0f;
+    colour = colour | (colour << 4);
+    colour = colour | (colour << 8);
+    colour = colour | (colour << 16); // 32 bits
+    int i = 0;
+    int end = (DISPLAY_WIDTH * DISPLAY_HEIGHT)/8;
+    while ( i < end )
+        framebuffer[i++] = colour;
+}
+
+// draw a horizontal line
+void vid_fill_line(int left, int right, int top, int colour){
+    int i;
+    for(i=left; i<right; i++){
+        vid_set_pixel(i, top, colour);
+    }
+}
+
+// fill a certain rectangle
+void vid_fill_rect(int left, int top, int right, int bottom, int colour){
+    volatile char *framebuffer = (volatile char *) (bufferaddr);
+    int width = right - left;
+    colour |= (colour << 4);
+    int y;
+    for (y=top; y<bottom; y++){
+        int i = (DISPLAY_WIDTH*y + left) / 2; // first bufferpos to fill
+        if (width > 1 && ((right & 1) != 0)){
+            // fill the 'odd half' of the end pixel
+            framebuffer[i+(width/2)] = (framebuffer[i+(width/2)] & 0xf0) | (colour & 0x0f);
+        }
+        
+        if ((left & 1) != 0) {
+            // fill the 'odd half' of the start pixel
+            framebuffer[i] = (framebuffer[i] & 0x0f) | (colour & 0xf0);
+            i++;
+        }
+        
+        int endpos = (DISPLAY_WIDTH*y + right)/2;
+        while (i < endpos)
+            framebuffer[i++] = colour;
+    }
+}
---
+++ Hardware/gp_socfpga/software/renderer/src/main.c
@@ -0,0 +1,84 @@
+#include "avalon_addr.h"
+#include "framebuffer.h"
+
+#define WIDTH   (480)
+#define HEIGHT  (272)
+
+// Types of work
+#define TYPE_SOF    (1)
+#define TYPE_EOF    (2)
+#define TYPE_LINE   (3)
+#define TYPE_RECT   (4)
+#define TYPE_TRI    (5)
+
+struct workorder {
+    char type;
+    char size;
+    char data[8];
+};
+
+// Number of work orders which can be queued.
+// 1024 bytes / sizeof(struct workorder), rounded down to nearest power of 2
+#define WORK_QUEUE_SIZE     (64)
+
+struct workqueue {
+    short doneptr;
+    short readyptr;
+    struct workorder orders[WORK_QUEUE_SIZE];
+};
+
+/*
+ * ARM writes the readyptr to mark the end of valid work orders.
+ * We write the doneptr to mark the next piece of work we have to do.
+ */
+int main(void){
+    volatile struct workqueue *queue = (struct workqueue *)(SHARED_BASE);
+    while(1){
+    // read from queue, perform work orders
+    while(queue->readyptr == queue->doneptr); // wait here while empty
+    
+    for(int i=queue->doneptr; i!=queue->readyptr; i = (i+1) & (WORK_QUEUE_SIZE - 1) ){
+        
+        // read the work order and do it
+        
+        volatile struct workorder order = queue->orders[i];
+        volatile short *data = (short *)(&order.data);
+        short x1,x2,y1,y2,col;
+        unsigned int addr;
+        switch(order.type){
+            case TYPE_SOF:
+                // read address of which buffer to use
+                addr = *(unsigned int *)(&order.data); // TODO use buffer number instead?
+                vid_setbuffer(addr);
+                vid_clear(0); //TODO hardware fast-clear?
+                break;
+            case TYPE_EOF:
+                // TODO: End of frame, mark framebuffer as locked
+                break;
+            case TYPE_LINE:
+                // read coords, color, fill pixels
+                x1 = data[0];
+                x2 = data[1];
+                y1 = data[2];
+                col = data[3];
+                vid_fill_line(x1, x2, y1, col);
+                break;
+            case TYPE_RECT:
+                // fill the rect
+                x1 = data[0];
+                y1 = data[1];
+                x2 = data[2];
+                y2 = data[3];
+                col = data[4];
+                vid_fill_rect(x1, y1, x2, y2, col);
+                break;
+            case TYPE_TRI:
+                // TODO: draw the triangle
+                break;
+        }
+        
+        // advance the doneptr
+        queue->doneptr = (queue->doneptr + 1) & (WORK_QUEUE_SIZE - 1);
+    }
+}
+}
