4148186947c007a7b27751ad234990960b5dad9e
Jamie Wood <jamie.wood70@ntlworld.com>
2016-10-18 17:33:56 +0100

    Performance increase by word-filling

--- Hardware/gp_socfpga/software/renderer/src/framebuffer.c
+++ Hardware/gp_socfpga/software/renderer/src/framebuffer.c
@@ -48,7 +48,9 @@ void vid_fill_line(int left, int right, int top, int colour){
     volatile char *framebuffer = (volatile char *) (bufferaddr);
     int width = right - left;
     colour |= (colour << 4);
+    
     int i = (DISPLAY_WIDTH*top + left) / 2; // first bufferpos to fill
+    
     if (width > 1 && ((right & 1) != 0)){
         // fill the 'odd half' of the end pixel
         framebuffer[i+(width/2)] = (framebuffer[i+(width/2)] & 0xf0) | (colour & 0x0f);
@@ -59,10 +61,46 @@ void vid_fill_line(int left, int right, int top, int colour){
         framebuffer[i] = (framebuffer[i] & 0x0f) | (colour & 0xf0);
         i++;
     }
-    
+        
     int endpos = (DISPLAY_WIDTH*top + right)/2;
-    while (i < endpos)
-        framebuffer[i++] = colour;
+    int len = endpos - i;
+    if(len < 12){ // if less than 3 words, copy bytes
+        while (i < endpos)
+            framebuffer[i++] = colour;
+        
+        return;
+    }
+    
+    int c32 = colour | (colour << 8); // extend to 32-bits
+    c32 |= (c32 << 16);
+    
+    unsigned int t;
+    
+    volatile char *dst = framebuffer + i;
+    // Align dst by filling in bytes
+    if ((t = (int)dst & 3) != 0){
+        t = 4 - t; // number of bytes to fill
+        len -= t;
+        do {
+            *dst++ = colour;
+        } while (--t != 0);
+    }
+    
+    // Fill as many full words as possible
+    t = len / 4;
+    do {
+        *(unsigned int *)dst = c32;
+        dst += 4;
+    } while (--t != 0);
+    
+    // Fill in any trailing bytes
+    t = len & 3;
+    if (t != 0){
+        do {
+            *dst++ = colour;
+        } while (--t != 0);
+    }
+    
 }
 
 // fill a certain rectangle
