aebc9a36fcd5d327908038f8039e54cb9b970514
Jamie Wood <jamie.wood70@ntlworld.com>
2017-02-18 22:33:22 +0000

    Updated testing script

--- Hardware/gp_socfpga/test.sh
+++ Hardware/gp_socfpga/test.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Try to download twice in case the JTAG connection is initializing the first time
-make -C software/renderer download || make -C software/render download
+function trydownload(){
+	make -C software/renderer download
+}
+
+# Try to download a few times in case the JTAG connection is initializing the first time
+n=0
+until [ $n -ge 5 ]
+do
+	trydownload && break  # substitute your command here
+	n=$[$n+1]
+	sleep 1
+done
+
+if [ $n -eq 5 ]
+then
+	echo "Failed to download to FPGA after 5 retries... Is it connected?"
+	exit 1
+fi
 
 # SSH into the ARM and run the test
 ssh jamie@172.31.60.16 -t 'sudo /home/jamie/run_test.sh'
