6ae449d0ee3220aa752776a1c537ba0782b944d1
Jamie Wood <jamie.wood70@ntlworld.com>
2017-03-31 16:05:32 +0100

    Update test.sh script

--- Hardware/gp_socfpga/test.sh
+++ Hardware/gp_socfpga/test.sh
@@ -4,20 +4,31 @@ function trydownload(){
 	make -C software/renderer download
 }
 
-# Try to download a few times in case the JTAG connection is initializing the first time
-n=0
-until [ $n -ge 5 ]
-do
-	trydownload && break  # substitute your command here
-	n=$[$n+1]
-	sleep 1
-done
+function dotest(){
+	algorithm=$1
+	echo $algorithm
+	make -C software/renderer ALGORITHM=ALGORITHM_$algorithm clean all update-mem
 
-if [ $n -eq 5 ]
-then
-	echo "Failed to download to FPGA after 5 retries... Is it connected?"
-	exit 1
-fi
+	# Try to download a few times in case the JTAG connection is initializing the first time
+	n=0
+	until [ $n -ge 5 ]
+	do
+		trydownload && break  # substitute your command here
+		n=$[$n+1]
+		sleep 1
+	done
 
-# SSH into the ARM and run the test
-ssh jamie@172.31.60.16 -t 'sudo /home/jamie/run_test.sh'
+	if [ $n -eq 5 ]
+	then
+		echo "Failed to download to FPGA after 5 retries... Is it connected?"
+		exit 1
+	fi
+
+	# SSH into the ARM and run the test
+	ssh jamie@192.168.0.16 -t 'cd /home/jamie/part2-project/Software && sudo ./run_test.sh'
+}
+
+dotest "BYTE_FILL"
+dotest "WORD_FILL"
+dotest "UNROLLED"
+dotest "LINE_FILLER"
